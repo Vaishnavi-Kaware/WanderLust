d1e152fbed4417785372ecbeeb414f3bb55d22aa
Vaishnavi Kaware <vkaware1212@gmail.com>
2026-03-18 12:18:37 +0530

    index route complete

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express ();
 const mongoose = require("mongoose");
+const Listing = require("./models/listing.js");
+const path = require("path");
 
 const MONGO_URL = 'mongodb://127.0.0.1:27017/wanderlust';
 
@@ -16,9 +18,33 @@ async function main() {
   await mongoose.connect(MONGO_URL);
 };
 
+app.set("view engine", "ejs");
+app.set("views",path.join(__dirname,"views"));
+
 app.get("/", (req, res)=>{
   res.send("Hi,I am rooot");
 });
+
+//index route
+app.get("/listings", async(req, res)=>{
+  const allListings = await Listing.find({});
+  res.render("listings/index.ejs",{allListings});
+});
+
+// app.get("/testListing", async(req, res) =>{
+//   let sampleListing = new Listing({
+//     title: "My new villa",
+//     description: "by the beach",
+//     price: 1200,
+//     location: "pune",
+//     country: "india",
+
+//   });
+//   await sampleListing.save();
+//   console.log("sample was saved");
+//   res.send("successful");
+  
+// })
 
 app.listen(8080,() =>{
   console.log("server is listening to port 8080");
--- models/listing.js
+++ models/listing.js
@@ -7,11 +7,23 @@ const listingSchema = new Schema({
     required: true,
   },
   description: String,
-  image: String,
+  image:{
+    type: String,
+    // filename: String,
+    // url: String,
+    default://image not provide image-undefined or null
+     "https://unsplash.com/photos/cozy-bedroom-with-wood-paneled-walls-and-a-view-0v7DOW9cT_o",
+
+    set: (v)=>//image aahe but link empty
+     v=="" ? 
+       "https://unsplash.com/photos/cozy-bedroom-with-wood-paneled-walls-and-a-view-0v7DOW9cT_o" 
+       : v,
+
+  },
   price: Number,
   location: String,
   country: String,
 });
 
 const Listing = mongoose.model("Listing", listingSchema);
-modules.export = Listing;
+module.exports = Listing;
