5eb13e67fcc146dc5e5526edcb3e28b08fec02c7
Vaishnavi Kaware <vkaware1212@gmail.com>
2026-03-18 12:41:36 +0530

    show  route complete

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ async function main() {
 
 app.set("view engine", "ejs");
 app.set("views",path.join(__dirname,"views"));
+app.use(express.urlencoded({extended:true}));
 
 app.get("/", (req, res)=>{
   res.send("Hi,I am rooot");
@@ -29,6 +30,15 @@ app.get("/", (req, res)=>{
 app.get("/listings", async(req, res)=>{
   const allListings = await Listing.find({});
   res.render("listings/index.ejs",{allListings});
+});
+
+//show route
+app.get("/listings/:id", async(req, res)=>{
+  let {id} =req.params;
+  id = id.trim(); 
+  const listing = await Listing.findById(id);
+  res.render("listings/show",{listing});
+  
 });
 
 // app.get("/testListing", async(req, res) =>{
