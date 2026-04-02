b22418d87b035a31f65d6f88d4612f4593c3deae
Vaishnavi Kaware <vkaware1212@gmail.com>
2026-04-02 10:53:29 +0530

    index file styling

--- models/listing.js
+++ models/listing.js
@@ -7,19 +7,31 @@ const listingSchema = new Schema({
     required: true,
   },
   description: String,
-  image:{
+  // image:{
+  //   type: String,
+  //   // filename: String,
+  //    //url: String,
+  //   default://image not provide image-undefined or null
+  //    "https://unsplash.com/photos/cozy-bedroom-with-wood-paneled-walls-and-a-view-0v7DOW9cT_o",
+
+  //   set: (v)=>//image aahe but link empty
+  //    v=="" ? 
+  //      "https://unsplash.com/photos/cozy-bedroom-with-wood-paneled-walls-and-a-view-0v7DOW9cT_o" 
+  //      : v,
+
+  // },
+
+
+image: {
+  filename: {
     type: String,
-    // filename: String,
-    // url: String,
-    default://image not provide image-undefined or null
-     "https://unsplash.com/photos/cozy-bedroom-with-wood-paneled-walls-and-a-view-0v7DOW9cT_o",
-
-    set: (v)=>//image aahe but link empty
-     v=="" ? 
-       "https://unsplash.com/photos/cozy-bedroom-with-wood-paneled-walls-and-a-view-0v7DOW9cT_o" 
-       : v,
-
+    default: "listingimage"
   },
+  url: {
+    type: String,
+    default: "https://images.unsplash.com/photo-1552733407-5d5c46c3bb3b"
+  }
+},
   price: Number,
   location: String,
   country: String,
